8f31af6eb5709077bba11fb3e54f1b07350cc126
Nico <nicoalesi.cs@gmail.com>
2025-05-13 08:49:22 +0200

    fix: improve exercise
    Fix exercise number 7 of chapter 2.
    Change the number of if statements used from 5 to 4.

--- Chapter05/Ex07.c
+++ Chapter05/Ex07.c
@@ -2,30 +2,40 @@
 
 
 int main(void) {
-    int n1, n2, n3, n4, smallest, largest;
+    int n1, n2, n3, n4;
+    int smallest1, largest1, smallest2, largest2;
+    int smallest, largest;
 
     printf("Enter four integers: ");
     scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
 
     if (n1 > n2) {
-        largest = n1;
-        smallest = n2;
+        largest1 = n1;
+        smallest1 = n2;
     } else {
-        largest = n2;
-        smallest = n1;
+        largest1 = n2;
+        smallest1 = n1;
     }
 
-    if (n3 > largest) {
-        largest = n3;
-    } else if (n3 < smallest) {
-        smallest = n3;
+    if (n3 > n4) {
+        largest2 = n3;
+        smallest2 = n4;
+    } else {
+        largest2 = n4;
+        smallest2 = n3;
     }
 
-    if (n4 > largest) {
-        largest = n4;
-    } else if (n4 < smallest) {
-        smallest = n4;
-    }   
+    if (smallest1 < smallest2) {
+        smallest = smallest1;
+    } else {
+        smallest = smallest2;
+    }
+
+    if (largest1 > largest2) {
+        largest = largest1;
+    } else {
+        largest = largest2;
+    }
 
     printf("Largest: %d\n", largest);
     printf("Smallest: %d\n", smallest);
